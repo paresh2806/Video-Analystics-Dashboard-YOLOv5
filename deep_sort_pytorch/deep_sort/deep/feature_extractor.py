e65cbc80dd204e1ca94992076c2559b60ce508f2
paresh2806 <81606923+paresh2806@users.noreply.github.com>
2022-07-04 15:02:12 +0530

    Update feature_extractor.py

--- deep_sort_pytorch/deep_sort/deep/feature_extractor.py
+++ deep_sort_pytorch/deep_sort/deep/feature_extractor.py
@@ -49,6 +49,6 @@ class Extractor(object):
 
 if __name__ == '__main__':
     img = cv2.imread("demo.jpg")[:, :, (2, 1, 0)]
-    extr = Extractor("checkpoint/ckpt.t7")
+    extr = Extractor("ckpt.t7")
     feature = extr(img)
     print(feature.shape)
